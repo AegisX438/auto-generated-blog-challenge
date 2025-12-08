637531271005474b2d2a0ece04385b7aaf363319
Ahmet Emin <gucluahmetemin@gmail.com>
2025-12-08 14:46:08 +0300

    Fix: API URL for to be dynamic

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+// Eğer tarayıcıdaki adres "localhost" ise localhost'u kullan,
+// Değilse (AWS'de ise) sunucunun IP adresini kullan.
+const hostname = window.location.hostname;
 const api = axios.create({
-    baseURL: "http://localhost:3000", // Backend adresimiz
+    baseURL: `http://${hostname}:3000`,
 });
 
 export default api;
